59e58da636b7da50cd0f087df7cbc58f19ce7219
iOS developer K <iOSdvlprK@gmail.com>
2023-03-06 21:38:00 +0900

    MVVM Card View Model

---
+++ SwipeMatchFS/ViewModels/CardViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  CardViewModel.swift
+//  SwipeMatchFS
+//
+//  Created by joe on 2023/03/06.
+//
+
+import UIKit
+
+struct CardViewModel {
+    // define the properties that view will display/render out
+    let imageName: String
+    let attributedString: NSAttributedString
+    let textAlignment: NSTextAlignment
+}
