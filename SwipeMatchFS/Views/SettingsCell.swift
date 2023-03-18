e716fe99c5f7e31e13b71a0671d54dc88a3d87fe
iOS developer K <iOSdvlprK@gmail.com>
2023-03-18 19:27:42 +0900

    settings form done

---
+++ SwipeMatchFS/Views/SettingsCell.swift
@@ -0,0 +1,50 @@
+//
+//  SettingsCell.swift
+//  SwipeMatchFS
+//
+//  Created by joe on 2023/03/18.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+    
+    class SettingsTextField: UITextField {
+        
+        override func textRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override func editingRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 24, dy: 0)
+        }
+        
+        override var intrinsicContentSize: CGSize {
+            return .init(width: 0, height: 44)
+        }
+    }
+    
+    let textField: UITextField = {
+        let tf = SettingsTextField()
+        tf.placeholder = "Enter Name"
+        tf.backgroundColor = .systemBackground
+        return tf
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+//        addSubview(textField)
+//        textField.fillSuperview()
+    }
+    
+    override func layoutSubviews() {
+        addSubview(textField)
+        textField.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
